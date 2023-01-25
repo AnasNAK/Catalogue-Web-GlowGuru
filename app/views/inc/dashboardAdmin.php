cbc56d0992cd1cf869c645859248a8905fa2d95b
AnasNAK <anasc00ding@gmail.com>
2023-01-25 16:15:51 +0100

    display sum stock

--- app/views/inc/dashboardAdmin.php
+++ app/views/inc/dashboardAdmin.php
@@ -1,9 +1,3 @@
-<?php
-if (!isLoggedIn()) {
-  redirect('Users/login');
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en" class="scroll-smooth">
 
@@ -33,8 +27,8 @@ if (!isLoggedIn()) {
           <img class="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="<?= URLROOT . '/img/Nakhli.jpg' ?>" />
           <span class="hidden md:block">anas nak</span>
         </div>
-        <div class="flex items-center h-14 bg-[#DBC4A1] w-3/4 ">
-          <ul class="flex items-center">
+        <div class="flex items-center justify-around h-14 bg-[#DBC4A1]  ">
+          <ul class="flex justify-between">
             <li>
               <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
             </li>
@@ -47,6 +41,20 @@ if (!isLoggedIn()) {
                   </svg>
                 </span>
                 Logout
+              </a>
+            </li>
+            <li>
+              <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
+            </li>
+            <li>
+              <a href="<?= URLROOT . '/Pages/index' ?> " class="flex  mr-4 hover:text-blue-100">
+                <span class="inline-flex mr-1">
+                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                    </path>
+                  </svg>
+                </span>
+                Go Back
               </a>
             </li>
           </ul>
@@ -81,9 +89,9 @@ if (!isLoggedIn()) {
       </div>
       <!-- ./Sidebar -->
 
-      <div class="h-full  mt-14 mb-10 md:ml-64">
+      <div class="h-full ml-14 mt-14 mb-10 md:ml-64">
         <!-- Statistics Cards -->
-        <div class="grid grid-cols-1  sm:grid-cols-2 gap-9 lg:grid-cols-2 p-4  ">
+        <div class="grid grid-cols-1  sm:grid-cols-2 gap-9 lg:grid-cols-3 p-4 lg:gap-32 ">
           <div class="bg-[#685942] shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-[#DBC4A1] text-white">
             <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
               <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-black transform transition-transform duration-500 ease-in-out">
@@ -96,19 +104,31 @@ if (!isLoggedIn()) {
               <p>Admin</p>
             </div>
           </div>
-          <div class="bg-[#685942] shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-[#DBC4A1] text-white">
+          <div class="bg-[#685942] shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-[#DBC4A1] text-white font-medium group">
             <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
               <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-black transform transition-transform duration-500 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z">
-                </path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path>
               </svg>
             </div>
             <div class="text-right">
-              <p class="text-2xl"><?= count($data['products']) ;?></p>
-              <p>product</p>
+              <p class="text-2xl"><?= count($data['products']); ?></p>
+              <p>Product</p>
             </div>
           </div>
-    
+          <div class="bg-[#685942] shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-[#DBC4A1] text-white font-medium group">
+            <div class="flex justify-center items-center w-14 h-14 bg-white rounded-full transition-all duration-300 transform group-hover:rotate-12">
+              <svg width="30" height="30" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-current text-black transform transition-transform duration-500 ease-in-out">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path>
+              </svg>
+            </div>
+
+            <div class="text-right">
+              <p class="text-2xl">
+                <?php print_r($data['productsum']->sum); ?>
+              </p>
+              <p>stock</p>
+            </div>
+          </div>
         </div>
         <div class="col-span-12 mt-5">
           <div class="grid gap-2 grid-cols-1 lg:grid-cols-1">
@@ -173,14 +193,14 @@ if (!isLoggedIn()) {
                                 </td>
                                 <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5">
                                   <div class="flex space-x-4">
-                                    <a href="<?= URLROOT; ?>/products/edit/<?= $Product->id;?>" class="text-blue-500 hover:text-blue-600">
+                                    <a href="<?= URLROOT; ?>/products/edit/<?= $Product->id; ?>" class="text-blue-500 hover:text-blue-600">
                                       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                       </svg>
                                       <p>Edit</p>
                                     </a>
 
-                                    <a href="<?= URLROOT; ?>/products/delete/<?= $Product->id;?>" class="text-red-500 hover:text-red-600">
+                                    <a href="<?= URLROOT; ?>/products/delete/<?= $Product->id; ?>" class="text-red-500 hover:text-red-600">
                                       <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 mr-1 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                       </svg>
@@ -190,7 +210,7 @@ if (!isLoggedIn()) {
                                 </td>
                               </tr>
                             </tbody>
-                          <?php endforeach ;?>
+                          <?php endforeach; ?>
                         </table>
                       </div>
                     </div>
